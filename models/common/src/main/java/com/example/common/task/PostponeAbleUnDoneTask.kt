2ff2fff561052ec987062451b1aee934d7e809be
hamuyatti <syota.takarabe@gmail.com>
2022-03-18 02:34:09 +0900

    延期できるtaskを表現するPostponeAbleUnDoneTaskを作成

---
+++ models/common/src/main/java/com/example/common/task/PostponeAbleUnDoneTask.kt
@@ -0,0 +1,15 @@
+package com.example.common.task
+
+class PostponeAbleUnDoneTask : UnDoneTask {
+    override val dueDate: Long
+        get() = TODO("Not yet implemented")
+
+    override fun doneTask() {
+        TODO("Not yet implemented")
+    }
+
+    override val id: Long
+        get() = TODO("Not yet implemented")
+    override val name: String
+        get() = TODO("Not yet implemented")
+}
